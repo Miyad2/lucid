19fa8fec8c2e61c0a908e9bd979f170e6d7008c6
Miyad2 <88500019+Miyad2@users.noreply.github.com>
2024-04-23 13:43:36 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function(){
+  
+  $('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    autoplay:false,
+    slidesToScroll: 1,
+    arrows:false,
+    pauseOnHover: false,
+    
+  
+   responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+  
+  });
+})
